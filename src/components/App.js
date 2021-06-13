796769163984cd778d419291d55ccf932a73df22
Marta Fraga Rubio <martafragarubio@gmail.com>
2021-06-13 14:55:38 +0200

    rama changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "../stylesheets/App.css";
 import data from "../data/data.json";
 import PokeList from "./PokeList";
+import { useState } from "react";
 
 //console.log(data);
 const App = () => {
+  const [pokemons] = useState(data);
+
   return (
     <div className="pokContainer">
       <h1 className="pokListTitle">mi lista de Pokemon</h1>
-      <PokeList data={data} />
+      <PokeList data={pokemons} />
     </div>
   );
 };
